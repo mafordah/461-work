70fba79959b5c848ac4fc8bcb82bd7bd4e96f018
Miya Fordah <42814756+mafordah@users.noreply.github.com>
2020-10-19 23:32:03 -0600

    add working HW8

---
+++ HW-08/homework3_webpage/homework3/Login.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace homework3
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string username = Request.QueryString["username"];
+            txtLogin.Text = "Welcome, " + username + "<br />";
+
+        }
+        protected void Submit(object sender, EventArgs e)
+        {
+            //Response.Redirect("Login.aspx?username=" + txtUserName.Text + "&lastname=" + txtPassword.Text);
+            Response.Redirect("Login.aspx?username=" + txtUserName.Text );
+            HttpCookie userInfo = new HttpCookie("userInfo");
+            userInfo["username"] = txtUserName.Text;
+            userInfo["password"] = txtPassword.Text;
+            userInfo.Expires.Add(new TimeSpan(0, 1, 0));
+            Response.Cookies.Add(userInfo);
+            string userName = string.Empty;
+            string password = string.Empty;
+            userName = Request.Cookies["username"].Value;
+            password = Request.Cookies["password"].Value;
+            
+
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
---
+++ HW-08/homework3_webpage/homework3/Users.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace homework3
+{
+    public partial class Users : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Events myEvents = new Events();
+            // DataSet myDataSet = myEvents.getPeople();
+            DataSet myDataSet = myEvents.getPerson(5);
+            gvPeople.DataSource = myDataSet.Tables[0];
+            gvPeople.DataBind();
+        }
+    }
+}
